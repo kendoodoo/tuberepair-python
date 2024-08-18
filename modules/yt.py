cb7799593d46b15bfab9cd64200e3e3cc007f817
cheese <89833162+kendoodoo@users.noreply.github.com>
2024-08-18 20:22:46 +0700

    SpaceSaver you dare

--- modules/yt.py
+++ modules/yt.py
@@ -13,7 +13,7 @@ def hls_video(video_id):
         }},
         "videoId": video_id
     }
-
+    
     data = requests.post('https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc', json=json_data, headers=header_data).json()
     panda = requests.get(data["streamingData"]["hlsManifestUrl"]).text.split("\n")
     formatfilter = re.compile(r"^#EXT-X-STREAM-INF:BANDWIDTH=(?P<bandwidth>\d+),CODECS=\"(?P<codecs>[^\"]+)\",RESOLUTION=(?P<width>\d+)x(?P<height>\d+),FRAME-RATE=(?P<fps>\d+),VIDEO-RANGE=(?P<videoRange>[^,]+),AUDIO=\"(?P<audioGroup>[^\"]+)\"(,SUBTITLES=\"(?P<subGroup>[^\"]+)\")?")
@@ -56,3 +56,22 @@ def hls_video(video_id):
             panda[x+1] = ""
     panda = "\n".join(panda)
     return panda
+
+# play 360p
+def medium_quality_video_url(video_id):
+
+    header_data = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; Xbox; Xbox One) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10553"
+    }
+
+    json_data = {
+        "context": {"client": {
+            "clientName": "TVLITE",
+            "clientVersion": "2"
+        }},
+        "videoId": video_id
+    }
+
+    data = session.post('https://www.youtube.com/youtubei/v1/player?key=' + api_key, json=json_data, headers=header_data).json()
+
+    return data["streamingData"]['formats'][0]['url']
