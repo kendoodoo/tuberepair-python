cea7e4e3d83aefc461f18e73e19f016b4f6cf006
cheese <89833162+kendoodoo@users.noreply.github.com>
2024-08-21 16:58:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Version v0.0.1 (beta-3.5)
+# Version v0.0.2 (beta-1)
 from flask import Flask, request, redirect, send_file, render_template, Response
 from flask_compress import Compress
 from uuid import uuid4
